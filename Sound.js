e7630766daae5c66a03cae0702274c221f164c19
akahuku <akahuku@gmail.com>
2014-08-03 18:14:11 +0900

    enable Firefox to play a sound

--- Sound.js
+++ Sound.js
@@ -28,15 +28,16 @@
 	}
 
 	Sound.prototype = {
+		createAudio: function () {
+			return new Audio;
+		},
 		load: function (key, callback) {
 			var that = this;
 			var prefix = '';
 			var suffix = '';
 			var a = {
 				enabled: false,
-				// TODO: Addon SDK does not have Audio feature...
-				// HiddenFrame module may be able to used?
-				audio: new Audio 
+				audio: this.createAudio()
 			};
 
 			this.pool[key] = a;
@@ -87,7 +88,7 @@
 				}, {noCache:true}
 			);
 		},
-		play: function (key, opts) {
+		doPlay: function (key, opts) {
 			if (!(key in this.pool)) {
 				return this.load(key, function () {
 					this.play(key, opts);
@@ -109,11 +110,58 @@
 				a.play();
 			}
 			catch (e) {}
+		},
+		play: function () {
+			this.doPlay.apply(this, arguments);
 		}
 	};
 
+	function SoundJetpack () {
+		Sound.apply(this, arguments);
+
+		var that = this;
+
+		this.hiddenFrameReady = false;
+		this.blocked = null;
+
+		var hf = require('sdk/frame/hidden-frame');
+		this.hiddenFrame = hf.add(hf.HiddenFrame({
+			onReady: function () {
+				that.hiddenFrameReady = true;
+				if (that.blocked) {
+					that.play(that.blocked.key, that.blocked.opts);
+					that.blocked = null;
+				}
+			}
+		}));
+	}
+
+	SoundJetpack.prototype = Object.create(Sound.prototype, {
+		createAudio: {value: function () {
+			return this.hiddenFrame
+				.element.contentWindow
+				.document.createElement('audio');
+		}},
+		play: {value: function (key, opts) {
+			if (this.hiddenFrameReady) {
+				this.doPlay.apply(this, arguments);
+			}
+			else {
+				this.blocked = {
+					key: key,
+					opts: opts
+				};
+			}
+		}}
+	});
+
 	function create () {
-		return new Sound;
+		if (require('sdk/self')) {
+			return new SoundJetpack;
+		}
+		else {
+			return new Sound;
+		}
 	}
 
 	exports.Sound = create;
